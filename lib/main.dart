40103dd9d76ec529eb229611b7aadd0a4d10b32f
“deepu” <manideepakgampa21@gmail.com>
2025-05-18 20:05:44 +0530

    18052025-0805PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(body: Text("Hello GnaNs")),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("theGnaN's-VtracK"),
+          backgroundColor: const Color.fromARGB(255, 33, 101, 205),
+        ),
+        body: Container(
+          child: Text("Hello World!"),
+          color: const Color.fromARGB(255, 230, 130, 145),
+          height: 450,
+          width: 450,
+        ),
+      ),
+    );
+  }
 }
