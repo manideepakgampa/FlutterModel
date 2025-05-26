20871f61a750094de3d2ddc23b661959f5797465
“deepu” <manideepakgampa21@gmail.com>
2025-05-26 18:32:40 +0530

    26052025-0632PM

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ class MyApp extends StatelessWidget {
           backgroundColor: const Color.fromARGB(255, 33, 101, 205),
         ),
         body: Container(
-          child: Text("Hello World!"),
-          color: const Color.fromARGB(255, 230, 130, 145),
+          child: Text("Hello World! This is theGnaN's - VtracK"),
+          color: const Color.fromARGB(255, 133, 255, 196),
           height: 450,
           width: 450,
         ),
